2dea9a68029a83851e2681dc44c41cff3fe80ae3
willian.pereira <ti04@supernorte.com.br>
2026-01-28 14:41:19 -0300

    CTK3 - button e command

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode('dark')
 
+# Criação das funções de funcionalidades
+def validar_login():
+    usuario = campo_usuario.get()
+    senha = campo_senha.get()
+
+    # Verificação
+    if usuario == 'willian' and senha == '123456':
+        resultado_login.configure(text='Login realizado com sucesso!', text_color='green')
+    else:
+        resultado_login.configure(text="Usuário ou senha incorretos!", text_color='red')
+
+
 # Configuração da janela principal
 
 app = ctk.CTk()
@@ -24,8 +36,16 @@ label_senha = ctk.CTkLabel(app, text='Senha')
 label_senha.pack(pady=10)
 
 # entry da senha
-campo_senha = ctk.CTkEntry(app, placeholder_text="Digite sua senha")
+campo_senha = ctk.CTkEntry(app, placeholder_text="Digite sua senha", show='*')
 campo_senha.pack(pady=10)
+
+#button
+bota_login = ctk.CTkButton(app, text='Login', command=validar_login)
+bota_login.pack(pady=10)
+
+# campo feedback de login
+resultado_login = ctk.CTkLabel(app, text='')
+resultado_login.pack(pady=10)
 
 # Iniciar a aplicação
 app.mainloop()
