32783dd9e2bd3a4d97bc14231de8bd47c0a8ef4a
willian.pereira <ti04@supernorte.com.br>
2026-01-28 14:12:36 -0300

    CTK1

---
+++ app.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+
+# Configuração da aparência
+
+ctk.set_appearance_mode('dark')
+
+# Configuração da janela principal
+
+app = ctk.CTk()
+app.title("SISTEMA DE LOGIN")
+app.geometry('300x300')
+
+# Iniciar a aplicação
+app.mainloop()
