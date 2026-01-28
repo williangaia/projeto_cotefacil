1718d7357af8786c6dd0db67099896c52d4fcd05
willian.pereira <ti04@supernorte.com.br>
2026-01-28 14:27:55 -0300

    CTK2 - label e entry

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ app = ctk.CTk()
 app.title("SISTEMA DE LOGIN")
 app.geometry('300x300')
 
+# Criação dos campos
+# label do usuário
+label_usuario = ctk.CTkLabel(app, text='Usuário')
+label_usuario.pack(pady=10)
+
+#entry do usuário
+campo_usuario = ctk.CTkEntry(app, placeholder_text="Digite seu usuário")
+campo_usuario.pack(pady=10)
+
+# label da senha
+label_senha = ctk.CTkLabel(app, text='Senha')
+label_senha.pack(pady=10)
+
+# entry da senha
+campo_senha = ctk.CTkEntry(app, placeholder_text="Digite sua senha")
+campo_senha.pack(pady=10)
+
 # Iniciar a aplicação
 app.mainloop()
