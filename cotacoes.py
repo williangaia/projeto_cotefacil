da274d3cfba3bb1cbb0219ec6effa5f3d5e947fc
willian.pereira <ti04@supernorte.com.br>
2026-01-29 09:59:37 -0300

    cotacoes.py - criar esqueleto completo com consulta no banco

--- cotacoes.py
+++ cotacoes.py
@@ -35,16 +35,19 @@ class ConexaoBD:
             print(f"Erro inesperado ao fechar a conexão: {e}", exc_info=True)
 
 class CriarCSV():
-    def __init__(self, numero_cotacao):
+    def __init__(self, numero_cotacao, conexao: ConexaoBD):
         self.numero_cotacao = numero_cotacao
         self.diretorio = Path(__file__).parent
         self.nome_arquivo = f"cotacao_{numero_cotacao}.csv"
         self.caminho_esqueleto = self.diretorio / self.nome_arquivo
+        self.conexao = conexao
 
     def criar_esqueleto(self):
         try:
+            produtos = self.buscar_produtos_cotacao()
+
             with open(self.caminho_esqueleto, 'w', newline='', encoding='utf-8') as arquivo:
-                writer = csv.writer(arquivo, delimiter='\t')
+                writer = csv.writer(arquivo, delimiter=',')
                 #Primeira linha - vazia
                 writer.writerow(['','','','','',''])
 
@@ -56,26 +59,61 @@ class CriarCSV():
 
                 #Quarta linha - colunas
                 writer.writerow(['Seq','EAN','Descrição','Emb.','Prazo','Vlr. Custo' ])
-            
+
+                #Preencher com os produtos
+                for produto in produtos:
+                    seq = produto['SEQPRODUTO']
+                    ean = produto['CODIGOEAN']
+                    descricao = produto['DESCRICAO']
+                    embalagem = f"{produto['EMBALAGEM']}-{produto['QTDEMBALAGEM']}"
+                    prazo = 30
+                    valor_custo = '""'
+                    writer.writerow([seq, ean, descricao, embalagem, prazo, valor_custo])
+
+            #depois tirar esse print - só para teste
+            print(f"CSV criado e preenchido em {self.caminho_esqueleto}")
+            print(f"Total de {len(produtos)} produtos adicionados")
             print(f"Esqueleto salvo em {self.diretorio}")
+            return True
+        
         except Exception as e:
             print(f"Erro ao criar esqueleto do CSV: {e}")
+            return False
         
-    
+    def buscar_produtos_cotacao(self):
+        cursor = self.conexao.conexao.cursor
 
+        consulta_produtos_cotacao = f"""
+        SELECT seqcotacao, seqproduto, codigoean, descricao, embalagem, qtdembalagem
+        FROM MRLV_LISTACOTACAO a
+        WHERE a.seqcotacao = {self.numero_cotacao}
+        """
 
+        cursor.execute(consulta_produtos_cotacao)
+        colunas = [desc[0] for desc in cursor.description]
+        linhas = cursor.fetchall()
 
-
+        produtos =[]
+        for linha in linhas:
+            produto = dict(zip(colunas, linha))
+            produtos.append(produto)
+        
+        return produtos
 
 
 
 # BLOCO PRINCIPAL
-"""
-conexao = ConexaoBD()
-conexao.verifica_conexao()
-time.sleep(5)
-conexao.fechar_conexao()
-"""
 
-esqueleto = CriarCSV(202261)
-esqueleto.criar_esqueleto()
+if __name__ == "__main__":
+    try:
+        conexao = ConexaoBD()
+        if conexao.verifica_conexao():
+            esqueleto = CriarCSV(202261, conexao)
+            esqueleto.criar_esqueleto()
+        else:
+            print("Falha na conexão com banco de dados")
+    except Exception as e:
+        print(f"Erro durante a execução: {e}")
+    finally:
+        if 'conexao' in locals():
+            conexao.fechar_conexao()
