d5b632abef13a3b6c7ce421e499507b6047348d6
willian.pereira <ti04@supernorte.com.br>
2026-01-30 18:09:41 -0300

    Geração de arquivos completa

--- data_frame.py
+++ data_frame.py
@@ -44,11 +44,11 @@ class CotacaoRepository():
 
         consulta_produtos_cotacao = f"""
         SELECT
-            SEQPRODUTO AS Seq,
-            CODIGOEAN AS EAN,
-            DESCRICAO AS Descrição,
-            EMBALAGEM,
-            QTDEMBALAGEM
+            SEQPRODUTO AS seq,
+            CODIGOEAN AS ean,
+            DESCRICAO AS descricao,
+            EMBALAGEM AS embalagem,
+            QTDEMBALAGEM AS qtd_embalagem
         FROM MRLV_LISTACOTACAO C
         WHERE C.SEQCOTACAO = {self.numero_cotacao}
         """
@@ -58,9 +58,7 @@ class CotacaoRepository():
         linhas = cursor.fetchall()
 
         df = pd.DataFrame(linhas, columns=colunas)
-
-        #Conctenar as duas colunas em uma chamada Emb.
-        #df["Emb."] = df["EMBALAGEM"].astype(str) + "-" + df["QTDEMBALAGEM"].astype(str)
+        df.columns = df.columns.str.lower()
 
         return df
     
@@ -87,6 +85,7 @@ class CotacaoRepository():
         linhas = cursor.fetchall()
 
         df = pd.DataFrame(linhas, columns=colunas)
+        df.columns = df.columns.str.lower()
 
         return df
 
@@ -94,9 +93,8 @@ class TxtCotacaoParser:
     def __init__(self, caminho_arquivo: Path):
         self.caminho_arquivo = caminho_arquivo
 
-    def extrair_precos(self) -> dict:
+    def extrair_precos(self) -> dict[str, dict[str, str]]:
         precos_por_fornecedor = {}
-
         cnpj_atual = None
 
         with open(self.caminho_arquivo, encoding="utf-8") as arquivo:
@@ -134,50 +132,115 @@ class TxtCotacaoParser:
 
         return precos_por_fornecedor
     
+class CotacaoDataFrameService:
+
     @staticmethod
     def montar_df_cotacao_fornecedor(df_cotacao: pd.DataFrame, precos_fornecedor: dict) -> pd.DataFrame:
         df = df_cotacao.copy()
 
-        df["Vlr. Custo"] = df["EAN"].map(precos_fornecedor).fillna("0,00")
+        df["Vlr. Custo"] = df["ean"].map(precos_fornecedor).fillna("0,00")
 
         return df
+    
+    @staticmethod ####
+    def preparar_df_final( df: pd.DataFrame) -> pd.DataFrame: ####
+        df_final = df.copy()
+
+        df_final["Emb."] = (
+            df_final["embalagem"].astype(str) + "-" + df_final["qtd_embalagem"].astype(str) #####
+        )
+
+        #Prazo - valor fixo no momento
+        df_final["Prazo"] = 30
+
+        # Formatar o preço - Vlr. Custo
+        df_final["Vlr. Custo"] = (
+            df_final["Vlr. Custo"].astype(str).str.replace(".", ",", regex=False)
+        )
+
+        # Formatar colinas do df
+        df_final = df_final[
+            ["seq", "ean", "descricao", "Emb.", "Prazo", "Vlr. Custo"]
+        ]
+
+        return df_final
+
+class CotacaoExporter:
+
+    @staticmethod ####
+    def salvar_cotacao_csv(df: pd.DataFrame, caminho: Path, numero_cotacao: int):
+
+        with open(caminho, mode="w", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+
+            # Cabeçalho Consinco
+            writer.writerow(["", "", "", "", "", ""])
+            writer.writerow([f"Cotação: {numero_cotacao}", "", "", "", "", ""])
+            writer.writerow(["CENTRAL-COMPRAS", "", "", "", "", ""])
+            writer.writerow(df.columns.tolist())
+
+            for _, row in df.iterrows():
+                writer.writerow(row.tolist())
+    
+    @staticmethod ####
+    def salvar_cotacao_xlsx(caminho: Path, dfs_por_fornecedor: dict[str, pd.DataFrame]):
+
+        with pd.ExcelWriter(caminho, engine="xlsxwriter") as writer:
+            for nome_razao, df in dfs_por_fornecedor.items():
+                aba = nome_razao[:31]
+                df.to_excel(writer, sheet_name=aba, index=False) ####
+
 
 # BLOCO PRINCIPAL
 
 if __name__ == "__main__":
+    
+    conexao = ConexaoBD()
+
     try:
-        conexao = ConexaoBD()
-        if conexao.verifica_conexao():
-            repositorio = CotacaoRepository(202280, conexao)
-            df_cotacao = repositorio.buscar_produtos_cotacao()
-            df_atacadistas = repositorio.buscar_atacadistas_cotacao()
+        if not conexao.verifica_conexao():
+            raise RuntimeError("Falha na conexão com o banco!")
+        
+        repositorio = CotacaoRepository(202280, conexao)
+        df_cotacao = repositorio.buscar_produtos_cotacao()
+        df_atacadista = repositorio.buscar_atacadistas_cotacao()
 
-            print("Produtos: ")
-            print(df_cotacao.head())
+        parser = TxtCotacaoParser(
+            Path(__file__).parent / "PEDIDO_13808028_29012026_0904532720.txt"
+        )
+        precos = parser.extrair_precos()
 
-            print("\nAtacadistas: ")
-            print(df_atacadistas.head())
+        dfs_xlsx = {}
 
-            diretorio_pai = Path(__file__).parent
-            arquivo = Path("PEDIDO_13808028_29012026_0904532720.txt")
-            caminho_txt = diretorio_pai / arquivo
+        for _, atac in df_atacadista.iterrows():
+            cnpj = atac["cnpj_completo"]
+            nome_razao = atac["nomerazao"]
 
-            parser = TxtCotacaoParser(caminho_txt)
-            precos = parser.extrair_precos()
-
-            for cnpj, precos_fornecedor in precos.items():
-                df_fornecedor = TxtCotacaoParser.montar_df_cotacao_fornecedor(
-                    df_cotacao,
-                    precos_fornecedor
-                )
-
-                print(f"\nFornecedor {cnpj}")
-                print(df_fornecedor.head())
+            df_fornecedor = CotacaoDataFrameService.montar_df_cotacao_fornecedor(
+                df_cotacao,
+                precos.get(cnpj, {})
+            )
             
-        else:
-            print("Falha na conexão com banco de dados")
-    except Exception as e:
-        print(f"Erro durante a execução: {e}")
+            df_final = CotacaoDataFrameService.preparar_df_final(df_fornecedor)
+
+            caminho_csv = (
+                Path(__file__).parent / f"Cotação{repositorio.numero_cotacao}_{nome_razao}.csv"
+            )
+
+            CotacaoExporter.salvar_cotacao_csv(
+                df_final,
+                caminho_csv,
+                repositorio.numero_cotacao
+            )
+
+            dfs_xlsx[nome_razao] = df_final
+
+        caminho_xlsx = (
+            Path(__file__).parent / f"Cotacao{repositorio.numero_cotacao}.xlsx"
+        )
+
+        CotacaoExporter.salvar_cotacao_xlsx(caminho_xlsx, dfs_xlsx)
+        
     finally:
         if 'conexao' in locals():
             conexao.fechar_conexao()
